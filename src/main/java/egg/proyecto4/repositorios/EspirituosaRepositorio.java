9a0fd3c3e998b3f67ddd6de0c81bb5ab011a2da5
ro-lucero <ro_lucero@hotmail.com>
2021-11-17 23:54:21 -0300

    repo con extends

--- src/main/java/egg/proyecto4/repositorios/EspirituosaRepositorio.java
+++ src/main/java/egg/proyecto4/repositorios/EspirituosaRepositorio.java
@@ -1,10 +1,11 @@
+
 package egg.proyecto4.repositorios;
 
-import egg.proyecto4.entidades.Compra;
+import egg.proyecto4.entidades.Espirituosa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompraRepositorio extends JpaRepository <Compra ,String>  {
+public interface EspirituosaRepositorio extends JpaRepository <Espirituosa ,String>  {
 
 }
