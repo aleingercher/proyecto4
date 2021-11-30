f8a210585c77be18fa0681ae57aaddda61e6d785
Alejandro Ingercher <aleingercher@gmail.com>
2021-11-29 23:44:56 -0300

    Revert "agg controllers Producto y Usuario"

--- src/main/java/egg/proyecto4/controllers/MainController.java
+++ src/main/java/egg/proyecto4/controllers/MainController.java
@@ -1,22 +1,9 @@
 package egg.proyecto4.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
 public class MainController {
 
-	@GetMapping("")
-	public String home() {
-		
-		return"Home";
-	}
-	
-	@GetMapping("/home")
-	public String homee() {
-		
-		return "Home";
-	}
+    // ingrese contenido aqui
 }
