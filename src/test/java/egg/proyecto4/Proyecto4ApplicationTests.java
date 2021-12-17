0910fa0b0ad0c4466025c5026461b0409f39a4a7
Sebas Navarrete <cs.navarrete@outlook.com>
2021-12-16 21:22:27 -0300

    cambio de body en home-admin

--- src/test/java/egg/proyecto4/Proyecto4ApplicationTests.java
+++ src/test/java/egg/proyecto4/Proyecto4ApplicationTests.java
@@ -1,13 +1,13 @@
-package egg.proyecto4;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Proyecto4ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package egg.proyecto4;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Proyecto4ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
